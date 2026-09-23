0ed7d8fd3cf44a6bf7c12d0f538f172f85f006c2
omaryossife-prog <omaryossife@gmail.com>
2026-09-24 00:35:34 +0300

    Create factory function for Prisma client
    Refactor Prisma client initialization to use a factory function.

--- server/src/utils/prisma.ts
+++ server/src/utils/prisma.ts
@@ -1,7 +1,12 @@
-import { PrismaClient } from "../generated/prisma/client.js";import { PrismaPg } from "@prisma/adapter-pg";
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
-});
+import { PrismaClient } from "../generated/prisma/client.js";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-export const prisma = new PrismaClient({ adapter });
+export function createPrisma(databaseUrl: string) {
+  const adapter = new PrismaPg({
+    connectionString: databaseUrl,
+  });
+
+  return new PrismaClient({ adapter });
+}
+
 export type Db = PrismaClient | Prisma.TransactionClient;
