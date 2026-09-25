9dbafd549bea849720218d9b52f63c10765db9c6
omaryossife-prog <omaryossife@gmail.com>
2026-09-25 16:33:56 +0300

    Delete wts-pay-main - Copy directory

--- wts-pay-main - Copy/server/src/middleware/rateLimit.middleware.ts
+++
@@ -1,9 +0,0 @@
-import type { RequestHandler } from "express";
-
-const passThrough: RequestHandler = (_req, _res, next) => {
-  next();
-};
-
-export const apiLimiter = passThrough;
-export const authLimiter = passThrough;
-export const transferLimiter = passThrough;
--- wts-pay-main - Copy/server/src/routes/referral.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { requireAuth } from "../middleware/auth.middleware.js";
-import { referralOverviewController } from "../controllers/referral.controller.js";
-
-const router = Router();
-router.use(requireAuth);
-router.get("/", referralOverviewController);
-export default router;
