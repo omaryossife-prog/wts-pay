247ce062d5f6f19bce42f82ca810dc5f7bd5e3a7
omaryossife-prog <omaryossife@gmail.com>
2026-09-25 05:33:34 +0300

    Delete wts-pay-main directory

--- wts-pay-main/server/src/utils/logger.ts
+++
@@ -1,5 +0,0 @@
-export const logger = {
-  info: (...args: unknown[]) => console.log(new Date().toISOString(), "[info]", ...args),
-  warn: (...args: unknown[]) => console.warn(new Date().toISOString(), "[warn]", ...args),
-  error: (...args: unknown[]) => console.error(new Date().toISOString(), "[error]", ...args),
-};
